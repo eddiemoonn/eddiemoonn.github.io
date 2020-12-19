8c70050010561f276f275deeba55dd38c413b04f
Benedicta <nnennaanih1@gmail.com>
2020-12-19 23:06:08 +0200

    last commit

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -8,10 +8,13 @@ export const ToDoForm = (props) => {
     const finish = (values) => {
         onSubmit(values.name, values.description)
     }
+    const onFinishFailed=(errorInfo)=>{
+        console.log('Failed:', errorInfo);
+    }
 
     return(
-        <Form className={'todo-form'} layout={'horizontal'} onFinish={finish}>
-            <Item name={'name'} label="Title" >
+        <Form className={'todo-form'} layout={'horizontal'} onFinish={finish} onFinishFailed={onFinishFailed}>
+            <Item name={'name'} rules={[{ required: true, message: 'Please input title!'}]} label="Title" >
                 <Input pattern="^[A-Z][a-zA-Z0-9]{2,}" title="3 characters minimum, must start with capital letter" />
             </Item>
             <Item name={'description'} label="Description">
