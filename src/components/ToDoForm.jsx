72ba0ef5719b25dc83b08104590e3dca47d08573
Benedicta <nnennaanih1@gmail.com>
2020-12-24 17:45:48 +0200

    the

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -15,10 +15,10 @@ export const ToDoForm = (props) => {
     return(
         <Form className={'todo-form'} layout={'horizontal'} onFinish={finish} onFinishFailed={onFinishFailed}>
             <Item name={'name'} rules={[{ required: true, message: 'Please input title!'}]} label="Title" >
-                <Input pattern="^[A-Z][a-zA-Z0-9]{2,}" title="3 characters minimum, must start with capital letter" />
+                <Input pattern="^[A-Z][a-zA-Z0-9]{2,}" title="3 characters minimum, must start with capital letter"/>
             </Item>
             <Item name={'description'} label="Description">
-                <Input pattern="^[A-Z][a-zA-Z0-9]{2,}" title="3 characters minimum, must start with capital letter" /> 
+                <Input pattern= "^.+\s.+$" /> 
             </Item>
             <Item>
                 <Button htmlType={'submit'}>Add</Button>
